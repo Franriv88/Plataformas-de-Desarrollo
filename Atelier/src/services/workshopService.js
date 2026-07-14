66513ae4b79818666aaf073f3989bb505b4fb54d
Francisco Rivas <frivasv2388@gmail.com>
2026-07-13 22:27:34 -0300

    separación de servicios: CardCrate y Workshps
    Al igual que con el manejo del array de usuarios por parte de Login.jsx y de TopBar.jsx, creé un archivo workshopSerivce.js dentro de la carpeta services.
    En dicho archivo están las funciones de getWorkshops(), saveWorkshp() y deleteWorkshp() que usaré en CardCreate.jsx y Workshp.jsx
    
    Se emplea localStorage acá y NO en cada componente

---
+++ Atelier/src/services/workshopService.js
@@ -0,0 +1,25 @@
+const STORAGE_KEY = "workshopList" //por convención al ser una constante de configuración usa SCREAMING_NAKE_CASE
+//fuente: https://www.flino.dev/blog/09-convenciones-codigo-case/ 
+
+export function getWorkshops() {
+     return JSON.parse(localSTorage.getItem(STORAGE_KEY)) || []
+}
+
+export function saveWorkshop(formData, editIndex) {
+    const existente = getWorkshops()
+    
+    if(editIndex != null){
+        const actualizado = existente.map((item, i) => i === editIndex ? formData : item)
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(actualizado))
+    }else{
+        existente.push(formData)
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(existente))
+    }
+}
+
+export function deleteWorkshop(indexToDelete){
+    const acutalizado = getWorkshops().filter((item, i) => i != indexToDelete)
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(actualizado))
+    
+    return actualizado
+}
